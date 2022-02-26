e8f72de6a062f313030ae412db1d33f3e23efb23
Anupam-dagar <dagaranupam@gmail.com>
2022-02-27 00:04:32 +0530

    migration: create user_privileges table.

---
+++ migrations/migration_queries/1645899213_UserPrivileges.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE user_privileges (
+    id varchar(255) NOT NULL DEFAULT "",
+    user_id varchar(255) NOT NULL DEFAULT "",
+    level_for ENUM('organisation', 'project') NOT NULL DEFAULT "project",
+    level_id varchar(255) NOT NULL DEFAULT "",
+    is_read TINYINT(1) NOT NULL DEFAULT 1,
+    is_write TINYINT(1) NOT NULL DEFAULT 0,
+    is_update TINYINT(1) NOT NULL DEFAULT 0,
+    is_delete TINYINT(1) NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_by varchar(255) NOT NULL DEFAULT "",
+    updated_by varchar(255) NOT NULL DEFAULT "",
+    PRIMARY KEY (id)
+) ENGINE=InnoDB
