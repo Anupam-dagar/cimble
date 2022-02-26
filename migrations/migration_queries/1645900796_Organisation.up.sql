61780b536f5d5065c3381433de0674750ead41f1
Anupam-dagar <dagaranupam@gmail.com>
2022-02-27 00:12:29 +0530

    migration: create organisation table.

---
+++ migrations/migration_queries/1645900796_Organisation.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE organisation (
+    id varchar(255) NOT NULL DEFAULT "",
+    name varchar(255) NOT NULL DEFAULT "",
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_by varchar(255) NOT NULL DEFAULT "",
+    updated_by varchar(255) NOT NULL DEFAULT "",
+    PRIMARY KEY (id)
+) ENGINE=InnoDB
