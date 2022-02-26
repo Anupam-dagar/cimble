2379bd0456794ac0339f10031d1a139be8418176
Anupam-dagar <dagaranupam@gmail.com>
2022-02-27 00:21:29 +0530

    migration: create configuration_archive table.

---
+++ migrations/migration_queries/1645901422_ConfigurationArchive.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE configuration_archive (
+    id varchar(255) NOT NULL DEFAULT "",
+    name varchar(255) NOT NULL DEFAULT "",
+    info varchar(255) NOT NULL DEFAULT "",
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_by varchar(255) NOT NULL DEFAULT "",
+    updated_by varchar(255) NOT NULL DEFAULT "",
+    deleted_timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_by varchar(255) NOT NULL DEFAULT "",
+    PRIMARY KEY (id)
+) ENGINE=InnoDB
