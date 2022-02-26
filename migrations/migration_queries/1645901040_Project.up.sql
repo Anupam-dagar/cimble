e67cbe40d3d46c43e030c32fa1ee47f7743bb0e1
Anupam-dagar <dagaranupam@gmail.com>
2022-02-27 00:16:09 +0530

    migration: create project table.

---
+++ migrations/migration_queries/1645901040_Project.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE project (
+    id varchar(255) NOT NULL DEFAULT "",
+    organisation_id varchar(255) NOT NULL DEFAULT "",
+    name varchar(255) NOT NULL DEFAULT "",
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_by varchar(255) NOT NULL DEFAULT "",
+    updated_by varchar(255) NOT NULL DEFAULT "",
+    PRIMARY KEY (id)
+) ENGINE=InnoDB
