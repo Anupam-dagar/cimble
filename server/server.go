a963be6a1e697f2abdce00904c2e56854eb0c939
Anupam-dagar <dagaranupam@gmail.com>
2022-03-03 00:45:04 +0530

    add graceful shutdown to server.

--- server/server.go
+++ server/server.go
@@ -3,13 +3,50 @@ package server
 import (
 	"cimble/server/router"
 	"cimble/utilities"
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func SetupServer() {
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
 	router := router.SetupRoutes()
 
 	port := utilities.GetEnvironmentVariableString("PORT")
-	router.Router.Run(fmt.Sprintf(":%s", port))
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: router.Router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	fmt.Println("Server exiting")
 }
