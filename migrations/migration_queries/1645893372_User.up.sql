7fea65682cd93b4c3d4c12314c5600263d6dee6c
Anupam-dagar <dagaranupam@gmail.com>
2022-03-05 13:48:10 +0530

    remove organisation id from user entity.

--- migrations/migration_queries/1645893372_User.up.sql
+++ migrations/migration_queries/1645893372_User.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE user (
     id varchar(255) NOT NULL,
-    organisation_id varchar(255) NOT NULL,
     first_name varchar(255) NOT NULL,
     last_name varchar(255),
     email varchar(255) NOT NULL,
