8becb9bf5b38683b2539004c0058fa25c7af343e
Anupam-dagar <dagaranupam@gmail.com>
2022-03-08 23:43:10 +0530

    update user_privileges primary key.

---
+++ migrations/migration_queries/1646762691_AlterUserPrivilegesPrimaryKey.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE user_privileges DROP PRIMARY KEY;
+ALTER TABLE user_privileges ADD PRIMARY KEY (user_id);
+
+ALTER TABLE user_privileges_archives DROP PRIMARY KEY;
+ALTER TABLE user_privileges_archives ADD PRIMARY KEY (user_id);
